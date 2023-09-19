ab0fdfe7c872d3a2bb9968cf51857263a4f20252
mubinanusratullayeva <mubinanusratullayeva1.@gmail.com>
2023-09-19 08:05:27 -0700

    pcodewars 1 example

--- lesson.py
+++ lesson.py
@@ -264,3 +264,12 @@ ilm_fandagi_shaxslar = {
 #
 # for i in range(5):
 #     print(i)
+
+
+# def first_non_consecutive(arr):
+#     for i in range(1, len(arr)):
+#         if arr[i] != arr[i-1] + 1:
+#             return arr[i]
+#         return None
+#
+# print(first_non_consecutive([1,2,3,4,6,7,8]))
